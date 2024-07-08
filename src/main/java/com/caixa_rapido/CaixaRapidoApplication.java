bd7e0516cb47458d0a4b4dd92bf63a87f9cb7333
Jonnas <jonnas.oliveira@sptech.school>
2024-07-08 17:26:15 -0300

    init commit

---
+++ src/main/java/com/caixa_rapido/CaixaRapidoApplication.java
@@ -0,0 +1,13 @@
+package com.caixa_rapido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaixaRapidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaixaRapidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caixa_rapido/CaixaRapidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caixa_rapido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaixaRapidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
